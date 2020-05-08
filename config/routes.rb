25637680c2914b6656b0d4bb75f48727cba8a6c5
Cutdownrecords <murasergio@gmail.com>
2020-05-08 23:01:50 +0200

    abilitato force ssl

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
-  get 'password_resets/new'
-  get 'password_resets/edit'
  root    'pages#home'
-  get 	 'contattaci'    => 'pages#contattaci'
-  get 	 'production'   => 'pages#production'
-  get 	 'dovesiamo' => 'pages#dovesiamo'
-  get 	 'signup'  => 'users#new'
-  get    '/login'   => 'sessions#new'
-  post   'login'   => 'sessions#create'
-  delete 'logout'  => 'sessions#destroy'
+  get    'contattaci' => 'pages#contattaci'
+  get    'production' => 'pages#production'
+  get    'dovesiamo'  => 'pages#dovesiamo'
+  get    'signup'     => 'users#new'
+  get    '/login'     => 'sessions#new'
+  post   'login'      => 'sessions#create'
+  delete 'logout'     => 'sessions#destroy'
   resources :users
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
